c2c58d6f8a4ef5129eedbf9bd1392ef9515f41e0
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-12 20:55:47 -0500

    make ButtonCounter component to place multiple

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
 import "./App.css";
-import useCount from "./hooks/useCount";
+import ButtonCounter from "./components/ButtonCounter";
 
 function App() {
-  const [count, increment, reset] = useCount();
-
   // <> is shorthand for <Fragment>
   return (
     <>
       <h1 className="mt-8 text-center uppercase text-red-600">
         Button Counter
       </h1>
-      <p className="my-8 text-center text-5xl">{count}</p>
-      <button
-        type="button"
-        className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
-        onClick={increment}
-      >
-        Click Me!
-      </button>
-      <button
-        type="button"
-        className="rounded bg-orange-500 py-2 px-4 font-bold text-black hover:bg-orange-700"
-        onClick={reset}
-      >
-        Reset
-      </button>
+
+      <ButtonCounter />
+      <ButtonCounter />
+      <ButtonCounter />
+      <ButtonCounter />
     </>
   );
 }
---
+++ src/components/ButtonCounter.jsx
@@ -0,0 +1,27 @@
+import useCount from "../hooks/useCount";
+
+function ButtonCounter() {
+  const [count, increment, reset] = useCount();
+
+  return (
+    <>
+      <p className="my-8 text-center text-5xl">{count}</p>
+      <button
+        type="button"
+        className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+        onClick={increment}
+      >
+        Click Me!
+      </button>
+      <button
+        type="button"
+        className="rounded bg-orange-500 py-2 px-4 font-bold text-black hover:bg-orange-700"
+        onClick={reset}
+      >
+        Reset
+      </button>
+    </>
+  );
+}
+
+export default ButtonCounter;
