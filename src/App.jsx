bf510c3c0f530f16d5c847174a7ed6c0639680b1
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-19 19:09:17 -0500

    Add id key values for buttons list.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import ButtonCounter from "./components/ButtonCounter";
 
 const buttons = [
   {
+    id: 1,
     txt: "Button 1",
   },
   {
+    id: 2,
     txt: "Button 2",
   },
   {
+    id: 3,
     txt: "Button 3",
   },
 ];
@@ -22,8 +25,8 @@ function App() {
         Button Counter
       </h1>
 
-      {buttons.map((crntBtn) => (
-        <ButtonCounter txt={crntBtn.txt} />
+      {buttons.map(({ txt, id }) => (
+        <ButtonCounter txt={txt} key={id} />
       ))}
     </>
   );
